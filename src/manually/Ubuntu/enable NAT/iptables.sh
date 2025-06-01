41038e759598fec4ec7cf974c04ec12bdbb29881
DarkStar <pegasus@quantasource.dev>
2025-05-31 23:41:00 -0500

    0

--- src/manually/Ubuntu/enable NAT/iptables.sh
+++ src/manually/Ubuntu/enable NAT/iptables.sh
@@ -1,5 +1,7 @@
 # https://wiki.archlinux.org/title/Internet_sharing#With_iptables
+#>Read the iptables article for more information (especially saving the rule and applying it automatically on boot). There is also an excellent guide on iptables Simple stateful firewall.
 
+# This doesn't persist on Reboot. To Do!
 internet0=enp1s0
 net0=ens1
 

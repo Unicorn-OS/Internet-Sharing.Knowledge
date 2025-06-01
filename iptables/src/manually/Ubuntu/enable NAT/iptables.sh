9ede09d3ce8b2410d29438b6fd42c1e60eb0b3fb
DarkStar <pegasus@quantasource.dev>
2025-06-01 01:56:45 -0500

    0

--- iptables/src/manually/Ubuntu/enable NAT/iptables.sh
+++ iptables/src/manually/Ubuntu/enable NAT/iptables.sh
@@ -6,5 +6,6 @@ internet0=enp1s0
 net0=ens1
 
 iptables -t nat -A POSTROUTING -o $internet0 -j MASQUERADE
+# implements Connection Tracking https://en.wikipedia.org/wiki/Netfilter#Connection_tracking
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i $net0 -o $internet0 -j ACCEPT
